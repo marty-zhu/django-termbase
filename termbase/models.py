dc8dede22d89389f2ad265565231df84ee714f59
marty-zhu <marty.zhu@gmail.com>
2023-10-07 15:02:36 +0000

    added model outline

--- termbase/models.py
+++ termbase/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+from datetime import datetime, timedelta
+
+
+class Client(models.Model):
+    pass
+
+
+class Topic(models.Model):
+    pass
+
+
+class Domain(models.Model):
+    pass
+
+
+class Term(models.Model):
+    KNOWLEDGE_TYPE = ()
+    REGISTER = ()
+    pass
