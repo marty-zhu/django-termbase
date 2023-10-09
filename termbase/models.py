cfb61ffe969f5009c945aa8ce649e77d3b61c7c8
marty-zhu <marty.zhu@gmail.com>
2023-10-09 20:39:02 +0000

    added base client model

--- termbase/models.py
+++ termbase/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 from django.utils import timezone
 
@@ -6,7 +7,37 @@ from datetime import datetime, timedelta
 
 
 class Client(models.Model):
-    pass
+    """Name of the end client the terminology is associated to.
+    """
+    parent = models.ForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+    ) 
+    name = models.CharField(
+        max_length=100,
+    )
+    abbr = models.Charfield(
+        max_length=10,
+        blank=True,
+        null=True
+    )
+    description = models.CharField(
+        max_length=300,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        ordering = ['parent', 'name']
+
+    def __str__(self):
+        """Human-readable string representation for validation."""
+
+    def get_absolute_url(self):
+        return reverse("termbase:view-clients", kwargs={"pk": self.pk})
+    
 
 
 class Topic(models.Model):
